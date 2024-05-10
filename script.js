bf011c675628056f324dc1a1f121a0aba6809f29
leidc024 <119598253+leidc024@users.noreply.github.com>
2024-05-10 22:24:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,21 +18,7 @@ var startTid = -1;      // ID of start transition
 
 
 
-function toggleInstructions() {
-    // Get the instruction menu element
-    var instructionMenu = document.getElementById("instructionMenu");
 
-    // Show the instruction menu by setting its display style to "block"
-    instructionMenu.style.display = "block";
-}
-
-function closeInstructions() {
-    // Get the instruction menu element
-    var instructionMenu = document.getElementById("instructionMenu");
-
-    // Hide the instruction menu by setting its display style to "none"
-    instructionMenu.style.display = "none";
-}
 
 
 class Stack { 
